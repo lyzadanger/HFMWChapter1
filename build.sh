572d99502552c7319461e1dc15364f08853f6ce2
Lyza Danger Gardner <lyza@lyza.com>
2011-10-16 11:43:39 -0700

    Stubbing out build script; moreso as test of deploy-via-remote than anything

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p chapter1
+cd chapter1
+#cp -LR ../01_wurfl_explore/01_begin explore
+#cp -LR ../02_panic_button/01_begin panic_button
+#mkdir -p device_classes
+#cp -LR ../03_device_class_test/01_begin device_classes/test_classes
